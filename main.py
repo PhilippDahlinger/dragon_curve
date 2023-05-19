03d9c92b19857d734dd1141e8eaa9cf339dcf07b
Philipp Dahlinger <philipp.dahlinger@kit.edu>
2023-05-19 13:00:40 +0200

    different drawing option

--- main.py
+++ main.py
@@ -8,44 +8,42 @@ from rendering import print_curve, plot_curve
 
 def plot_4_curves(d):
     fig, ax = plt.subplots()
-    # plot_curve(ax, d, color="blue", start_dir=0)
-    # plot_curve(ax, d, color="red", start_dir=1)
-    # plot_curve(ax, d, color="green", start_dir=2)
-    # plot_curve(ax, d, color="purple", start_dir=3)lue", start_dir=0)
-    plot_curve(ax, d, color="all", start_dir=0)
-    plot_curve(ax, d, color="all", start_dir=1)
-    plot_curve(ax, d, color="all", start_dir=2)
-    plot_curve(ax, d, color="all", start_dir=3)
+    plot_curve(ax, d, color="blue", start_dir=0)
+    plot_curve(ax, d, color="red", start_dir=1)
+    plot_curve(ax, d, color="green", start_dir=2)
+    plot_curve(ax, d, color="purple", start_dir=3)
+    # plot_curve(ax, d, color="all", start_dir=0)
+    # plot_curve(ax, d, color="all", start_dir=1)
+    # plot_curve(ax, d, color="all", start_dir=2)
+    # plot_curve(ax, d, color="all", start_dir=3)
     plt.show()
     plt.close()
 
 
+def plot_single_curve(d):
+    fig, ax = plt.subplots()
+    plot_curve(ax, d, color="all", start_dir=1)
+    plt.show()
+    plt.close()
+
+def animate_single_curve(d):
+    plt.ion()
+    plt.cla()
+    plot_curve(plt.gca(), d, color="all", start_dir=1)
+    plt.pause(1.0)
+
+
+
+
+
 if __name__ == '__main__':
     dragon_curves = [first_iteration()]
-    for i in range(100):
+    for i in range(20):
         dragon_curves.append(next_iteration(dragon_curves[-1]))
-        plot_4_curves(dragon_curves[-1])
-    #
-    # t = np.linspace(0, 10, 200)
-    # x = np.cos(np.pi * t)
-    # y = np.sin(t)
-    #
-    # # Create a set of line segments so that we can color them individually
-    # # This creates the points as a N x 1 x 2 array so that we can stack points
-    # # together easily to get the segments. The segments array for line collection
-    # # needs to be numlines x points per line x 2 (x and y)
-    # points = np.array([x, y]).T.reshape(-1, 1, 2)
-    # segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    #
-    # # Create the line collection object, setting the colormapping parameters.
-    # # Have to set the actual values used for colormapping separately.
-    # lc = LineCollection(segments, cmap=plt.get_cmap('copper'),
-    #                     norm=plt.Normalize(0, 10))
-    # lc.set_array(t)
-    # lc.set_linewidth(3)
-    #
-    # plt.gca().add_collection(lc)
-    # plt.xlim(-1, 1)
-    # plt.ylim(-1, 1)
-    # plt.show()
+        plot_single_curve(dragon_curves[-1])
+        # plot_4_curves(dragon_curves[-1])
+        # animate_single_curve(dragon_curves[-1])
+
+
+
 
